7cad6c6370f39b6c72af287616b4cfb976ae62d5
wslack11 <slack@riverdale.edu>
2016-08-09 14:17:59 +0000

    changed olympic medal file

---
+++ intro-to-ruby.rb
@@ -0,0 +1,29 @@
+# puts "hello" #string
+# puts 1 #integer
+# puts true #boolean
+
+#methods
+
+greeting = "Hello Sam"
+excited_sam = greeting.upcase + "!!"
+
+# puts greeting
+# puts excited_sam
+# puts excited_sam.downcase
+
+# put greeting.downcase.reverse
+
+# puts "What is your name?"
+# name = gets.chomp.capitalize
+# puts "Hello, #{name}"
+
+# lab = "upperline-find-missing-pet"
+# puts lab.gsub("-", " ")
+
+num = 20
+puts num % 2
+puts num % 3
+num2 = 21
+puts num2 % 2
+
+
---
+++ olympic-medal-count.rb
@@ -0,0 +1,21 @@
+puts "U.S. OLYMPIC MEDAL COUNT"
+
+num_gold = 5
+num_silver = 7
+num_bronze = 7
+
+total = num_gold + num_silver + num_bronze
+
+puts "The U.S. has won:"
+puts "Gold: #{num_gold}"
+puts "Silver: #{num_silver}"
+puts "Bronze: " + num_bronze.to_s
+puts "Total: #{total}"
+
+puts "swimming".capitalize
+puts "volleyball".upcase
+puts "target shooting".downcase
+puts "air rifle".reverse
+puts "fencing".length
+puts "diving".gsub("i","p")
+
